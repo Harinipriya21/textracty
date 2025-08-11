04eda0a731abbcaabc583f727e43473071257e46
Harini Priya <112061473+Harinipriya21@users.noreply.github.com>
2025-08-11 22:02:34 +0530

    Update About.jsx

--- src/block/About.jsx
+++ src/block/About.jsx
@@ -2,6 +2,12 @@ export default function About() {
   return (
     <div>
       <h1>About</h1>
+      <p>
+      This project focuses on the digitization and preservation of ancient manuscripts and old scripts by converting images of these documents into editable, machine-readable text.
+        Many historical texts are handwritten or printed in scripts that are difficult to read or have degraded over time.
+        By leveraging advanced OCR (Optical Character Recognition) technology, the project accurately extracts text from these images, enabling researchers and scholars to analyze and study them more effectively. 
+        Additionally, the project supports translating the extracted text into various modern languages, broadening accessibility and helping preserve cultural heritage. This effort aids in safeguarding valuable historical knowledge and making it widely available for education and research.
+      </p>
     </div>
   );
 }
